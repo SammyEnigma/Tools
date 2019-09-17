eea46a8ddba8c838d66d1a3e85cc0da4135b52f6
CuriousPeng <CuriousPeng@outlook.com>
2019-09-17 10:56:45 +0800

    封装

---
+++ JsonToEntity/Business/JavaParseEntity.cs
@@ -0,0 +1,31 @@
+﻿using JsonToEntity.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JsonToEntity.Business
+{
+    public class JavaParseEntity : IJavaEntityRepository
+    {
+        public string GetLanguage()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetPath()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetTemplate()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Dictionary<Type, string> GetTypeMap()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
