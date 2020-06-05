fc665940f93ec18f8f1beac35a1549b90f3d8204
Curious Peng <CuriousPeng@outlook.com>
2020-06-05 17:16:51 +0800

    添加demo,添加helper的interface

---
+++ Demo/Program.cs
@@ -0,0 +1,16 @@
+﻿using Demo.Redis;
+using System;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //RedisToolsUseDemo.UseDemo();
+            //Pub可以后运行，先运行Sub，运行多个都可以，输入字符串要一样，
+            RedisToolsUseDemo.Pub();
+            Console.Read();
+        }
+    }
+}
---
+++ Demo/Redis/RedisToolsUseDemo.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using RedisTools;
+
+namespace Demo.Redis
+{
+    public class RedisToolsUseDemo
+    {
+        private static RedisBase redisBase = new RedisBase("localhost:6379,password=123456,abortConnect=false,connectTimeout=10000");
+        private static RedisHelper redisHelper = new RedisHelper(redisBase);
+        public static void UseDemo()
+        {
+            var data = new A
+            {
+                a = "test",
+                b = "test for redis",
+                c = 1,
+                d = 0.2d,
+                f = DateTime.Now,
+                g = true
+            };
+            var aKey = "akey";
+            redisHelper.Add(aKey, data);//添加
+            var dataB = redisHelper.Get<A>(aKey);//获取
+            Console.WriteLine(dataB.a);
+            redisHelper.Remove(aKey);//删除
+            Console.WriteLine(redisHelper.Exists(aKey));//是否存在
+        }
+
+        public static async Task Pub()
+        {
+            Console.WriteLine("请输入要向那个通道发送消息？");
+            var channel = Console.ReadLine();
+
+            await Task.Delay(10);
+            for (int i = 0; i < 10; i++)
+            {
+                await redisHelper.PublishAsync(channel, i.ToString());
+            }
+
+        }
+    }
+    public class A
+    {
+        public string a;
+        public string b;
+        public int c;
+        public double d;
+        public DateTime f;
+        public bool g;
+    }
+}
